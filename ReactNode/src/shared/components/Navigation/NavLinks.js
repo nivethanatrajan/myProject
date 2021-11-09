4f6b3b4f095b29450ef8a46bf3277fb465b72d1d
nivethanatrajan <nivethanatrajan26@gmail.com>
2021-11-09 19:09:55 +0530

    navigation bar

---
+++ ReactNode/src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./NavLinks.css";
+import { NavLink } from "react-router-dom";
+
+const NavLinks = (props) => {
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/">ALL USERS</NavLink>
+      </li>
+      <li>
+        <NavLink to="/u1/places">MY PLACES</NavLink>
+      </li>
+      <li>
+        <NavLink to="/place/new">ADD PLACE</NavLink>
+      </li>
+      <li>
+        <NavLink to="/auth">AUTHENTICATE</NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default NavLinks;
